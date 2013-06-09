4e936cfeb0c70a9a396ce05666e555e624826b4c
cameel <cameel2@gmail.com>
2013-06-09 02:11:16 +0200

    Add widgets: date picker, search box and a hard-coded list of empty notes

--- main_window.py
+++ main_window.py
@@ -1,12 +1,31 @@
-from PyKDE4.kdeui   import KMainWindow, KDatePicker
-from PyQt4.QtGui    import QWidget, QLineEdit, QVBoxLayout, QHBoxLayout, QScrollArea, QTextEdit
+""" The main UI component of the application. Controls the whole window """
 
-class MainWindow(object):
+from PyKDE4.kdeui import KMainWindow, KDatePicker
+from PyQt4.QtGui  import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QScrollArea, QTextEdit
+
+class MainWindow(KMainWindow):
     def __init__(self):
-        self.main_panel = QWidget()
+        super().__init__()
 
-        self.window = KMainWindow()
-        self.window.setCentralWidget(self.main_panel)
+        self.main_panel        = QWidget()
+        self.left_panel        = QWidget(self.main_panel)
+        self.date_picker       = KDatePicker(self.left_panel)
+        self.left_panel_layout = QVBoxLayout(self.left_panel)
+        self.left_panel_layout.addWidget(self.date_picker)
+        self.left_panel_layout.addStretch()
 
-    def show(self):
-        self.window.show()
+        self.right_panel   = QWidget(self.main_panel)
+        self.search_box    = QLineEdit(self.right_panel)
+        self.note_area     = QScrollArea(self.right_panel)
+
+        right_panel_layout = QVBoxLayout(self.right_panel)
+        right_panel_layout.addWidget(self.search_box)
+        right_panel_layout.addWidget(self.note_area)
+
+        note_area_layout = QVBoxLayout(self.note_area)
+
+        self.main_panel_layout = QHBoxLayout(self.main_panel)
+        self.main_panel_layout.addWidget(self.left_panel)
+        self.main_panel_layout.addWidget(self.right_panel)
+
+        self.setCentralWidget(self.main_panel)
