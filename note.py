87d0679d981fc9d629555449b22e2f9aa94000dd
cameel <cameel2@gmail.com>
2013-06-11 02:24:37 +0200

    Note: Change timestamp format
    - isoformat() sometimes omits microseconds making the string harder to
    parse

--- note.py
+++ note.py
@@ -1,6 +1,10 @@
 """ A class that represents a single note """
 
+from datetime import datetime
+
 class Note:
+    TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
     def __init__(self, title, body, tags, timestamp):
         self.title     = title
         self.body      = body
@@ -12,5 +16,7 @@ class Note:
             'title':     self.title,
             'body':      self.body,
             'tags':      self.tags,
-            'timestamp': self.timestamp.isoformat()
+            # NOTE: isoformat() is supposedly faster but it's output is harder
+            # to parse as the microsecond part is omitted if it's 0.
+            'timestamp': self.timestamp.strftime(self.TIMESTAMP_FORMAT)
         }
