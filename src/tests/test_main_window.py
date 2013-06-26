a74658b5414b40c6d5d55db66079295980c7776f
cameel <cameel2@gmail.com>
2013-06-26 23:46:37 +0200

    MainWindow: Create a new TapeWidget when loading notes
    - This prevents it from keeping it the old state like for example search
    box content
     - Unlike before the tape is not cleared but kept open until a new one
    is successfully created. If loading notes fails, the old tape is still
    kept open.

---
+++ src/tests/test_main_window.py
@@ -0,0 +1,48 @@
+import unittest
+import sys
+from datetime import datetime
+
+from PyQt4.QtTest import QTest
+from PyQt4.QtGui  import QApplication
+
+from ..main_window import MainWindow
+from ..tape_widget import TapeWidget
+from ..note        import Note
+
+class TapeWidgetTest(unittest.TestCase):
+    def setUp(self):
+        self.application = QApplication(sys.argv)
+        self.window      = MainWindow()
+
+    def tearDown(self):
+        # FIXME: Python crashes without this. Probably unittest does not destroy the old instance of the
+        # class before creating a new one and PyQt does not like having two instances of QApplication?
+        self.application = None
+
+    def test_replace_tape_widget_should_destroy_old_tape_and_put_a_new_one_on_the_panel(self):
+        old_tape_widget   = self.window.tape_widget
+        main_panel_layout = self.window.main_panel_layout
+
+        assert isinstance(old_tape_widget, TapeWidget)
+        assert 0 <= main_panel_layout.indexOf(old_tape_widget) < main_panel_layout.count()
+
+        old_tape_widget.add_note()
+        old_tape_widget.add_note()
+        old_note1 = old_tape_widget._note_widgets[0].note
+        old_note2 = old_tape_widget._note_widgets[1].note
+
+        new_note = Note(
+            title     = "X",
+            body      = "Y",
+            tags      = ["Z"],
+            timestamp = datetime.utcnow()
+        )
+
+        self.window._replace_tape_widget([new_note])
+
+        self.assertTrue(isinstance(self.window.tape_widget, TapeWidget))
+        self.assertNotEqual(self.window.tape_widget, old_tape_widget)
+        self.assertEqual(len(self.window.tape_widget._note_widgets), 1)
+        self.assertEqual(self.window.tape_widget._note_widgets[0].note, new_note)
+        self.assertTrue(0 <= main_panel_layout.indexOf(self.window.tape_widget) < main_panel_layout.count())
+        self.assertEqual(self.window.tape_widget.parentWidget(), self.window.main_panel)
