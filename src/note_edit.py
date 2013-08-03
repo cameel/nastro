c4cedd371beb86169b87fdb8b29e1348b9649218
cameel <cameel2@gmail.com>
2013-08-04 00:16:55 +0200

    AutoResizingTextEdit(): Override heightForWidth()

--- src/note_edit.py
+++ src/note_edit.py
@@ -15,10 +15,43 @@ class AutoResizingTextEdit(QTextEdit):
     def __init__(self, parent = None):
         super().__init__(parent)
 
+        # This seems to have no effect. I have expected that it will cause self.hasHeightForWidth()
+        # to start returning True, but it hasn't - that's why I hardcoded it to True there anyway.
+        # I still set it to True in size policy just in case - for consistency.
+        size_policy = self.sizePolicy()
+        size_policy.setHeightForWidth(True)
+        self.setSizePolicy(size_policy)
+
         # If we don't call adjustSize() here, document's height() keeps returning 0 even
         # if text has been set with setPlainText(). It returns the actual height
         # only after user modified the text through the UI.
         self.document().adjustSize()
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        margins = self.contentsMargins()
+
+        if width >= margins.left() + margins.right():
+            document_width = width - margins.left() - margins.right()
+        else:
+            # If specified width can't even fit the margin, there's no space left for the document
+            document_width = 0
+
+        # Cloning the whole document only to check its size at different width seems wasteful
+        # but apparently it's the only and preferred way to do this in Qt >= 4. QTextDocument does not
+        # provide any means to get height for specified width (as some QWidget subclasses do).
+        # Neither does QTextEdit. In Qt3 Q3TextEdit had working implementation of heightForWidth()
+        # but it was allegedly just a hack and was removed.
+        #
+        # The performance probably won't be a problem here because the application is meant to
+        # work with a lot of small notes rather than few big ones. And there's usually only one
+        # editor that needs to be dynamically resized - the one having focus.
+        document = self.document().clone()
+        document.setTextWidth(document_width)
+
+        return margins.top() + document.size().height() + margins.bottom()
 
     def sizeHint(self):
         margins          = self.contentsMargins()
